4daddba64b0a83e224168388f2975e405782a3b0
sobiaf <sobia.furqan@cushon.co.uk>
2022-09-23 10:07:05 +0100

    adding final files for a session to take place. Including data for not only setting the session, but also for destroying the current session

--- htdocs/session_data.php
+++ htdocs/session_data.php
@@ -1,1 +1,27 @@
+<?php session_start(); ?>
+
 <?php
+public function kill_session()
+{
+    unset($_SESSION['username']);
+    unset($_SESSION['password']);
+    session_destroy();
+    echo '<hr>Session Destroyed<br>';
+    echo 'Session ID: '.session_id().'<br>';
+//    var_dump($_SESSION);
+}
+if (count($_SESSION) > 0)
+{
+    echo '<dl>';
+    foreach($_SESSION as $key => $value)
+    {
+        echo "<dt>Key: $key";
+        echo "<dd>Value: $value";
+    }
+    echo '<dl><hr>';
+    echo 'Session ID: '.session_id().'<br>';
+    var_dump($_SESSION);
+    kill_session();
+} else {
+    echo 'Please <a href="session_form.html">Login</a>';ß
+}
